3b17a068120cd7b06a2bec3f81f0bf226d659cc8
egamovj <egamovj90@gmail.com>
2024-05-14 21:44:07 +0500

    add & remove item

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,59 @@
 /* eslint-disable no-unused-vars */
-import { getData } from './constants/db';
-import Card from './components/card/card';
+import { getData } from "./constants/db";
+import Card from "./components/card/card";
 import "./App.css";
-import Cart from './components/cart/cart';
+import Cart from "./components/cart/cart";
+import { useState } from "react";
 
-const courses = getData()
+const courses = getData();
 
 const App = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAddItem = (item) => {
+    const existItem = cartItems.find((c) => c.id == item.id);
+
+    if (existItem) {
+      const newData = cartItems.map((c) =>
+        c.id == item.id ? { ...existItem, quantity: existItem.quantity + 1 } : c
+      );
+      setCartItems(newData);
+    } else {
+      const newData = [...cartItems, { ...item, quantity: 1 }];
+      setCartItems(newData);
+    }
+  };
+
+  const onRemoveItem = (item) => {
+    const existItem = cartItems.find((c) => c.id == item.id);
+
+    if (existItem.quantity === 1) {
+      const newData = cartItems.filter((c) => c.id !== existItem.id);
+      setCartItems(newData);
+    } else {
+      const newData = cartItems.map((c) =>
+        c.id === existItem.id
+          ? { ...existItem, quantity: existItem.quantity - 1 }
+          : c
+      );
+      setCartItems(newData);
+    }
+  };
+
   return (
     <>
-      <h1 className='heading'>Course</h1>
+      <h1 className="heading">Course</h1>
       <Cart />
       {/* Card */}
       <div className="cards__container">
-        {courses.map((course)=> (
+        {courses.map((course) => (
           <>
-            <Card key={course.id}  course={course}/>
+            <Card
+              key={course.id}
+              course={course}
+              onAddItem={onAddItem}
+              onRemoveItem={onRemoveItem}
+            />
           </>
         ))}
       </div>
--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./button.css";
 
 const Button = (props) => {
-  const { type, title } = props;
+  const { type, title, onClick } = props;
   return (
     <button
       className={`btn ${
@@ -10,6 +10,7 @@ const Button = (props) => {
         (type === "remove" && "remove") ||
         (type === "checkout" && "checkout")
       }`}
+      onClick={onClick}
     >
       {title}
     </button>
